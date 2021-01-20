7a59ab47d57be7120832ff675f791e03f22ed2dd
Sagor <mbrsagor@gmail.com>
2021-01-20 22:52:58 +0600

    Type API added paginations

--- contacts/views/type_view.py
+++ contacts/views/type_view.py
@@ -1,10 +1,18 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.pagination import PageNumberPagination
 from contacts.models.types import Type
 from contacts.serializers.type_serializer import TypeSerializer
+
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 8
 
 
 class TypeViewSet(ModelViewSet):
     queryset = Type.objects.all()
     serializer_class = TypeSerializer
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [AllowAny, ]
+    pagination_class = StandardResultsSetPagination
--- homework/settings.py
+++ homework/settings.py
@@ -123,6 +123,9 @@ CORS_ORIGIN_WHITELIST = (
 
 # Rest framework settings
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication'
     ],
