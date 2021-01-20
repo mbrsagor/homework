bd7c76178f1088f5b01bf519dcd928897d8e1081
Sagor <mbrsagor@gmail.com>
2021-01-20 11:10:30 +0600

    Default user registration permissions allow any user

--- contacts/views/auth_view.py
+++ contacts/views/auth_view.py
@@ -1,11 +1,14 @@
 from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework import permissions
+
 from contacts.serializers.auth_serializer import RegisterSerializer, UserSerializer
 
 
 # Register API
 class RegisterApiView(generics.GenericAPIView):
     serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
